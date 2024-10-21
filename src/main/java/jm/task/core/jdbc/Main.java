6423f99580ca5b28e33eaf98fc209a35c3a7349c
Norton <metalhydrogenp@gmail.com>
2024-10-21 09:08:22 -0500

    that is all.

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -14,19 +14,19 @@ public class Main {
     public static void main(String[] args) {
         UserServiceImpl usi = new UserServiceImpl();
 usi.createUsersTable();
-usi.saveUser("ass","bigass",(byte)66);
-System.out.println("User с именем — ass добавлен в базу данных");
-usi.saveUser("29A666","cunt",(byte)99);
-        System.out.println("User с именем — 29A666 добавлен в базу данных");
-usi.saveUser("bender","rodrigas",(byte)100);
-        System.out.println("User с именем — bender добавлен в базу данных");
-usi.saveUser("lila","turanga",(byte)25);
-        System.out.println("User с именем — lila добавлен в базу данных");
+usi.saveUser("Lestat","Delionkur",(byte)125);
+System.out.println("User с именем — Lestat добавлен в базу данных");
+usi.saveUser("Akasha","Goddes",(byte)-1);
+        System.out.println("User с именем — Akasha добавлен в базу данных");
+usi.saveUser("Claudia","Rice",(byte)6);
+        System.out.println("User с именем — Claudia добавлен в базу данных");
+usi.saveUser("Anne","Rice",(byte)80);
+        System.out.println("User с именем — Anne добавлен в базу данных");
         List<User> ulist = usi.getAllUsers();
         for(int i=0; i< ulist.size();i++){
             System.out.println(ulist.get(i).toString());
         }
-        usi.removeUserById(1L);
+        usi.removeUserById(3L);
         usi.dropUsersTable();
         usi.dropUsersTable();
         usi.shutdown();
