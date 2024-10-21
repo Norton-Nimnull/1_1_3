81969a9d92bdafdda4fc2ce222bf682b454edf0a
Norton <metalhydrogenp@gmail.com>
2024-10-21 09:09:10 -0500

    that is all.

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -2,10 +2,7 @@ package jm.task.core.jdbc;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserServiceImpl;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+
 import java.util.List;
 
 public class Main {
